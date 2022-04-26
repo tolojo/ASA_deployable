6a522204af9a0c24843d4063e675fb5431675062
Tomás Ferreira <tomasferreira91@gmail.com>
2022-04-27 00:45:18 +0100

    a

--- models/connection.js
+++ models/connection.js
@@ -1,11 +1,15 @@
 var pg = require('pg');
 
-const connectionString = "postgres://postgres:postgres@localhost:5432/asa"
+const connectionString = "postgres://qgdztskwskqsnf:c8f2e5c9469627648df673e388c231fa8d02d2e91b8e97730f111877b798d3c9@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dev63vgku6a0rm"
 
 const Pool = pg.Pool
 const pool = new Pool({
     connectionString,
-    max: 20
+    max: 20,
+    ssl: {
+        require: true, 
+        rejectUnauthorized: false
+    }
     
 })
 
