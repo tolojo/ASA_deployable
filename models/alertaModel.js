74ccd3cdac2ff6e7232fbdfefa03dc08c280df9f
Tomás Ferreira <tomasferreira91@gmail.com>
2022-06-08 03:12:53 +0100

    Atualização

--- models/alertaModel.js
+++ models/alertaModel.js
@@ -21,7 +21,7 @@ module.exports.getAlertas = async function () {
 module.exports.getAlertaById = async function (id) {
     try {
         let sql = "Select * from alerta where alerta_id = $1";
-        let result = await pool.query(sql,[id]);
+        let result = await pool.query(sql, [id]);
         let alerta = result.rows[0];
         return {
             status: 200,
@@ -40,7 +40,7 @@ module.exports.getAlertaById = async function (id) {
 module.exports.saveAlerta = async function (alerta) {
     try {
         let sql = "insert into alerta(alerta_nome, alerta_descricao, alerta_localizacao_lat, alerta_localizacao_lng, alerta_pessoa_id, alerta_ta_id) values ($1,$2,$3,$4,$5,$6)";
-        let result = await pool.query(sql, [alerta.alerta_nome,alerta.alerta_descricao,alerta.alerta_localizacao_lat,alerta.alerta_localizacao_lng,alerta.alerta_pessoa_id,alerta.alerta_ta_id]);
+        let result = await pool.query(sql, [alerta.alerta_nome, alerta.alerta_descricao, alerta.alerta_localizacao_lat, alerta.alerta_localizacao_lng, alerta.alerta_pessoa_id, alerta.alerta_ta_id]);
         return {
             status: 200,
             result: result
@@ -59,13 +59,26 @@ module.exports.saveAlertaPsa = async function (alerta) {
     try {
         let sql1 = "select max(alerta_id) from alerta";
         let result1 = await pool.query(sql1, []);
+        let sql2 = "select max(psa_alerta_laerta_id) from psa_alerta";
+        let result2 = await pool.query(sql2, []);
         let alertaId = result1.rows[0];
+        let ultimoId = result2.rows[0];
         console.log(alertaId.max);
-        let sql = "insert into psa_alerta(psa_alerta_alerta_id,psa_alerta_psa_id) values ($1,$2)";
-        let result = await pool.query(sql, [alertaId.max,alerta.caracteristicas_alerta_psa_id]);
-        return {
-            status: 200,
-            result: result
+        if (alertaId.max != ultimoId.max) {
+            let sql = "insert into psa_alerta(psa_alerta_alerta_id,psa_alerta_psa_id) values ($1,$2)";
+            let result = await pool.query(sql, [alertaId.max, alerta.caracteristicas_alerta_psa_id]);
+            return {
+                status: 200,
+                result: result
+            }
+        }
+        else{
+            let sql = "insert into psa_alerta(psa_alerta_alerta_id,psa_alerta_psa_id) values ($1,$2)";
+            let result = await pool.query(sql, [alertaId.max + 1, alerta.caracteristicas_alerta_psa_id]);
+            return {
+                status: 200,
+                result: result
+            }
         }
     } catch (err) {
         console.log(err);
@@ -79,7 +92,7 @@ module.exports.saveAlertaPsa = async function (alerta) {
 module.exports.getTipoAlerta = async function () {
     try {
         let sql = "Select * from tipo_alerta";
-        let result = await pool.query(sql,[]);
+        let result = await pool.query(sql, []);
         let alerta = result.rows;
         return {
             status: 200,
